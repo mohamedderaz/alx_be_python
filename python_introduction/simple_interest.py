fd8286e4edf03007e38bbb10b52518e22f40cfba
mohamedderaz <deraz.mohamed11@gmail.com>
2025-05-21 09:21:05 +0300

    Add files via upload

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+P = 1000 # principal with a value of 1000 (representing $1000)
+R = 0.05 # rate with a value of 0.05 (representing 5% annual interest rate)
+T = 3 # time with a value of 3 (representing 3 years).
+I = P*R*T
+print ("principal ="+ str(P))
+print ("rate =" + str(R))
+print ("time = " + str(T))
+print ( "The simple interest is:"+ str(I))
