8fbf4facc08b8581461de859fa2c1227b0d9f1de
mohamedderaz <deraz.mohamed11@gmail.com>
2025-06-01 18:43:06 +0300

    pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,8 @@
 length = int(input("Enter the size of the pattern:"))
 while length >=1 :
     for width in range(1,length+1) :
-       print(width*"*",end="")     
-    break   
+       print("* ",end="")  
+    print()        
    
    
    
