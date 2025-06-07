85c33e55b53690a531a03a7852f3521603f7d324
mohamedderaz <deraz.mohamed11@gmail.com>
2025-06-07 17:04:28 +0300

    add temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -6,14 +6,32 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     fahrenheit = (celsius*CELSIUS_TO_FAHRENHEIT_FACTOR)+32
     print(f"{celsius} C is {fahrenheit} F")    
+
+def is_valid_temperature(value):
+    try:
+        float(value)  # نحاول نحول القيمة إلى رقم عشري
+        return True
+    except ValueError:
+        return False
+
+def is_valid_unit(unit):
+    return unit.upper() in ["C", "F"]
+
 temp = int(input("Enter the temperature to convert: "))
 type= str(input("Is this temperature in Celsius or Fahrenheit? (C/F):"))
-if type=="C":
-    convert_to_fahrenheit(temp)
-elif type=="F":
-    convert_to_celsius(temp)
-else:print("enter a valid temp and unit")
+# مدخلات من المستخد
 
+# التحقق من المدخلات
+if is_valid_temperature(temp) and is_valid_unit(type):
+    temperature = float(temp)
+    unit = type.upper()
+    if type=="C":
+        convert_to_fahrenheit(temp)
+    elif type=="F":
+        convert_to_celsius(temp)
+    else:print("enter a valid temp and unit")
+else:
+    print("Invalid input. Please enter a valid number and unit (C or F).")
 
     
 
